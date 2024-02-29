87bb6b53bcf5fac30dbf9287ad46b668ef56ea09
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-29 00:39:35 -0500

    code-along-complete

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useState } from "react";
 function Toggle() {
-  return <button>OFF</button>;
+  const [lightSwitch, setLightSwitch] = useState(false)
+
+  function handleClick(){
+    setLightSwitch((lightSwitch)=> !lightSwitch)
+  }
+
+  console.log(lightSwitch)
+
+   return <button style={{background: "red"}} onClick={handleClick}>{lightSwitch ? 'ON' : "OFF"}</button>;
 }
 
 export default Toggle;
